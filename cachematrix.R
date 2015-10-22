a985fb31d442462383faca7bf10696be35037c7b
Benedict Lee <lwywork@gmail.com>
2015-10-22 21:49:56 +0800

    Peer based Assessment (Matrix function)
    Peer based Assessment (Inverse of Matrix function)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache Matrix inversion functions rather than compute it repeatedly
+## Will write pair of functions to cache inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix will: 
+## a. set the value of the matrix
+## b. get the value of the matrix
+## c. get value of inverse of the matrix
+## d. set the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+inv <- NULL
 
+set <- function(y){
+  x<<- y
+  inv <<- NULL
+  
 }
 
+  get <- function()x
+  setinverse <- function(inverse) inv <<-inverse
+  getinverse <- function() inv
+  list(get=get, set=set, setinverse=setinverse, getinverse=getinverse )
+}
+  
 
-## Write a short comment describing this function
+
+## cacheSolve computes the inverse of the matrix returned by makeCahceMatrix. 
+## if the inverse is already cached, it will retrieve result from memory directly 
+## via the function, instead 
+## of calcuating. If not, inverse is calculated via setinverse function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinverse()
+  if (!is.null(inv)){
+    message("Cached data from memory will be fetched...")
+    return(inv)
+    
+  }
+  data<-x$get()
+  inv <-solve(data)
+  x$setinverse(inv)
+  inv
+  
 }
